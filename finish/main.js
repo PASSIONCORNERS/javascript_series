a23e988712caafd645994ea3f19c97c49e7aa1c0
Phong <44795008+PASSIONCORNERS@users.noreply.github.com>
2023-08-09 21:30:18 +0700

    [lesson_02] complete

---
+++ finish/main.js
@@ -0,0 +1,23 @@
+// Functions
+
+function greet(data) {
+  alert(data);
+}
+
+// greet("Hello Phong");
+
+function trippleMe(data) {
+  return 3 * data;
+}
+
+let myNumber = trippleMe(3);
+
+// console.log(myNumber);
+
+function doubleMe(data) {
+  return 2 * data;
+}
+
+let myNewNumber = doubleMe(2);
+
+console.log(myNewNumber);
