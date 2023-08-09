1fc817b90a7251a4069ea7480e4d9c51a9c7e411
Phong <44795008+PASSIONCORNERS@users.noreply.github.com>
2023-08-09 19:28:53 +0700

    [lesson_01] complete

---
+++ finish/main.js
@@ -0,0 +1,19 @@
+// Variables
+
+let myNumber = 6; // Number
+
+myNumber = 26;
+
+let myName = "Phong"; // String
+
+var myColor = "blue";
+
+const myAge = 36;
+
+// myAge = 21;
+
+// document
+
+console.log(myNumber);
+
+console.log(myAge);
