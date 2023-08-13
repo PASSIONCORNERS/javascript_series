10bec0190bef9226680ff12299aee9b87ca7003e
Phong <44795008+PASSIONCORNERS@users.noreply.github.com>
2023-08-13 14:26:37 +0700

    [lesson 05] finish

--- finish/main.js
+++ finish/main.js
@@ -1,1 +1,27 @@
 // If Else Statement
+
+let myStore = 200;
+
+let myNumber = 6;
+
+let myWord = "chip";
+
+let myOb = { name: "thy", email: "test@gmail.com" };
+
+// >= > =
+
+// <= < =
+
+// !
+
+// ! = =
+
+// if (myStore !== 200) {
+//   console.log("store open");
+// }
+
+let myBoo = myStore !== 200;
+
+console.log(typeof myNumber);
+console.log(typeof myWord);
+console.log(typeof myOb);
