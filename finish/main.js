7ac8da2fba15a2a9f3fdfb9641d085c9a99aed3d
Phong <44795008+PASSIONCORNERS@users.noreply.github.com>
2023-08-16 22:56:55 +0700

    [lesson_08] finish

--- finish/main.js
+++ finish/main.js
@@ -1,1 +1,26 @@
 // Scope
+
+// global
+let myName1 = "Phong";
+
+function myFunc() {
+  // local
+  let myName2 = "Thy";
+  console.log(myName1);
+}
+
+myFunc();
+
+console.log(myName1);
+
+// function myFunc2() {
+//   // local
+//   let myName3 = "Minh";
+//   console.log("test");
+//   function myFunc3() {
+//     // local
+//     console.log("test");
+//   }
+// }
+
+// myFunc();
