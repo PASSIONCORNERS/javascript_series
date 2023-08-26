022907e1f250a9e41776c6534719c213889a3ea7
Phong <44795008+PASSIONCORNERS@users.noreply.github.com>
2023-08-26 16:14:27 +0700

    [lesson 14] finish

--- finish/main.js
+++ finish/main.js
@@ -1,1 +1,34 @@
 // Express
+
+let express = require("express");
+
+let myApp = express();
+
+myApp.use(express.urlencoded({ extended: false }));
+
+myApp.get("/", function (req, res) {
+  res.send(`
+   <form action="/answer" method="POST">
+   <p>What is 2 + 2</p>
+   <input name="answer"></input>
+   <button>Submit</button>
+   </form>
+   `);
+});
+
+myApp.post("/answer", function (req, res) {
+  // res.send("TESTING SUCCESS");
+  if (req.body.answer == 4) {
+    res.send(`
+      <p>Correct Answer</p>
+      <a href="/">Return</a>
+      `);
+  } else {
+    res.send(`
+      <p>Wrong Answer</p>
+      <a href="/">Return</a>
+      `);
+  }
+});
+
+myApp.listen(3000);
