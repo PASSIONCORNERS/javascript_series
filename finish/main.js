dbd7ba35531b285fdbcc69dae9d1dc9c35d263fb
Phong <44795008+PASSIONCORNERS@users.noreply.github.com>
2023-08-26 15:41:46 +0700

    [lesson 13] finish

--- finish/main.js
+++ finish/main.js
@@ -1,1 +1,21 @@
 // Nodejs
+
+// let myNumber = 6;
+
+// let myResult = 6 + myNumber;
+
+// console.log(myResult);
+let http = require("http");
+
+let myServer = http.createServer(function (req, res) {
+  // console.log(req.url);
+  // res.end("Hello World");
+  if (req.url == "/") {
+    res.end("Hello World");
+  }
+  if (req.url == "/login") {
+    res.end("Hello User");
+  }
+});
+
+myServer.listen(3000);
